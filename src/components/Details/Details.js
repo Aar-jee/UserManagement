2008b668123e5062e64728dd831d0f24a2cf2733
Rohit Ghai <rohitghai91@gmail.com>
2018-04-28 22:11:00 +0530

    latest detail file

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 
 class Details extends Component {
   render() {
+    console.log(this.props.name);
     return (
       <div className="Details">
-        <h3>Details</h3>
+        <h3>Name:{this.props.name}</h3>
         <h4></h4>
-        <span><a href="/">Home</a></span>{' '}
+        <span><a href="/">Go to HomePage</a></span>{' '}
        </div>
     )
   }
